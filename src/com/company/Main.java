73e49c73c4149d7e9a04844a0b55caf054e8de4f
Misha <mihail.lazareff@gmail.com>
2020-06-21 13:12:45 -0400

    test2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test 123");
         System.out.println("Test 1234");
+        System.out.println("Test 12345");
 
     }
 }
